28201a2ee607745aef64299621876a6945ec5f59
olorunfemiayanfe@gmail.com <olorunfemiayanfe@gmail.com>
2025-04-02 15:48:47 +0100

    change configureservices

--- EcommerceData/obj/Debug/net5.0/EcommerceData.AssemblyInfo.cs
+++ EcommerceData/obj/Debug/net5.0/EcommerceData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca71d309ff45228009329c3802a95039d9b18d46")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
